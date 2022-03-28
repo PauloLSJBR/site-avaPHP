de0eafe7bb0626e80f783eb153cf537de110f995
PauloLSJBR <37719839+PauloLSJBR@users.noreply.github.com>
2022-03-28 10:20:12 -0300

    Add files via upload

---
+++ apiPHP/Classes/Validator/RequestValidator.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Validator;
+
+
+class RequestValidator
+{
+
+    public function __construct()
+    {
+        echo 'Testando autoload';
+    }
+
+}
---
+++ apiPHP/index.php
@@ -0,0 +1,25 @@
+<?php
+
+use Util\ConstantesGenericasUtil;
+use Util\JsonUtil;
+use Util\RotasUtil;
+use Validator\RequestValidator;
+
+require_once('bootstrap.php');
+
+phpinfo();
+
+try {
+    $RequestValidator = new RequestValidator(RotasUtil::getRotas());
+    $retorno = $RequestValidator->processarRequest();
+
+    $JsonUtil = new JsonUtil();
+    $JsonUtil->processarArrayParaRetornar($retorno);
+
+} catch (Exception $exception) {
+    echo json_encode([
+        ConstantesGenericasUtil::TIPO => ConstantesGenericasUtil::TIPO_ERRO,
+        ConstantesGenericasUtil::RESPOSTA => utf8_encode($exception->getMessage())
+    ], JSON_THROW_ON_ERROR, 512);
+    exit;
+}
